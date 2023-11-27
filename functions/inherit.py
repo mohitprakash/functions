ce3c70516de03f44945ff91a4984c1b122eb7c48
Mohit Prakash <mohitprakash88@gmail.com>
2023-11-27 13:45:37 +0530

    Code Commit

--- functions/inherit.py
+++ functions/inherit.py
@@ -1,1 +1,11 @@
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
 
+    def printName(self):
+        print(self.firstname, self.lastname)
+
+
+x = Person("John","Doe")
+x.printName()
