9afd8e138f40d4521801c66569b0be550abe9ee2
Mohit Prakash <mohitprakash88@gmail.com>
2023-11-29 00:08:51 +0530

    New File

---
+++ functions/py_List.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry", "apple"]
+print(thislist)
+print(len(thislist))
+
+# Duplicates are allowed
+
+list1= ["apple", "banana", "cherry", "apple"]
+list2= [1, 5, 7, 9, 3]
+list3= [True,False,True]
+
+print(list1, list2, list3)
